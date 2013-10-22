658cab94dfc9f619d3190280f764a0e72acc2e2e
dnotestein <dan@syncad.com>
2013-10-21 23:29:05 -0400

    Update contact completion model when a new contact is added or when a contact is changed.

--- AddressBook/AddressBookModel.cpp
+++ AddressBook/AddressBookModel.cpp
@@ -227,21 +227,38 @@ QVariant AddressBookModel::data( const QModelIndex& index, int role )const
 
 int AddressBookModel::storeContact( const Contact& contact_to_store )
 {
+   QModelIndex completionIndex;
    if( contact_to_store.wallet_index == WALLET_INVALID_INDEX )
    {
-       auto num_contacts = my->_contacts.size();
-       beginInsertRows( QModelIndex(), num_contacts, num_contacts );
+      auto num_contacts = my->_contacts.size();
+      beginInsertRows( QModelIndex(), num_contacts, num_contacts );
           my->_contacts.push_back(contact_to_store);
           my->_contacts.back().wallet_index =  my->_contacts.size()-1;
-       endInsertRows();
-       my->_address_book->store_contact( my->_contacts.back() );
-       return my->_contacts.back().wallet_index;
+      endInsertRows();
+      //update completion model with new contact dac_id and fullname
+      int row_count = my->_contact_completion_model.rowCount();
+      my->_contact_completion_model.insertRows( row_count, 2 );
+      completionIndex = my->_contact_completion_model.index(row_count);
+      my->_contact_completion_model.setData(completionIndex, contact_to_store.dac_id_string.c_str());
+      completionIndex = my->_contact_completion_model.index(row_count+1);
+      my->_contact_completion_model.setData(completionIndex, contact_to_store.getFullName().c_str());
+
+      //add fullname to completion list
+      my->_address_book->store_contact( my->_contacts.back() );
+      return my->_contacts.back().wallet_index;
    }
 
    FC_ASSERT( contact_to_store.wallet_index < int(my->_contacts.size()) );
    auto row = contact_to_store.wallet_index;
    my->_contacts[row] = contact_to_store;
    my->_address_book->store_contact(  my->_contacts[row]  );
+
+   //update completion model with modified contact dac_id and fullname
+   int completionRow = row * 2;
+   completionIndex = my->_contact_completion_model.index(completionRow);
+   my->_contact_completion_model.setData(completionIndex, contact_to_store.dac_id_string.c_str());
+   completionIndex = my->_contact_completion_model.index(completionRow+1);
+   my->_contact_completion_model.setData(completionIndex, contact_to_store.getFullName().c_str());
 
    Q_EMIT dataChanged( index( row, 0 ), index( row, NumColumns - 1) );
    return contact_to_store.wallet_index;
