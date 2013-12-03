5d9e2b3653f6dbd5821957f0c2fbaeed628003b3
vogel76 <wrona@syncad.com>
2013-12-03 12:00:20 +0100

    [BW]: [Ign] Added batches to simplify work on windows platform.

---
+++ run_cmake.bat
@@ -0,0 +1,3 @@
+call setenv.bat
+cd %INVICTUS_ROOT%
+cmake-gui -G "Visual Studio 11"
---
+++ setenv.bat
@@ -0,0 +1,15 @@
+@echo off
+set INVICTUS_ROOT=%~dp0..\
+echo Using %INVICTUS_ROOT% as Invictus root directory
+
+set QTDIR=%INVICTUS_ROOT%\QT
+set OPENSSL_ROOT=%INVICTUS_ROOT%\OpenSSL
+set OPENSSL_ROOT_DIR=%OPENSSL_ROOT%
+set OPENSSL_INCLUDE_DIR=%OPENSSL_ROOT%\include
+set ICU_ROOT=%INVICTUS_ROOT%\ICU
+
+set PATH=%QTDIR%\bin;%ICU_ROOT%\bin;%INVICTUS_ROOT%\bin;%INVICTUS_ROOT%\Cmake\bin;%PATH%
+
+echo Setting up VS2012 environment...
+call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat"
+
