2008700603aeabadc4190897e8deb1399ae8281e
grzegorzs <gszczepanek@syncad.com>
2014-06-25 10:24:38 +0200

    [GS] Modifications to integrate with bitshares_toolkit (install)

--- HelperScripts/jenkins_build.bat
+++ HelperScripts/jenkins_build.bat
@@ -47,10 +47,15 @@ rem popd to be in startup dir
 popd
 
 :doBld
+IF EXIST "%INVICTUS_ROOT%\bin" (
+  echo "Removing bin directory"
+  rmdir /Q /S "%INVICTUS_ROOT%\bin" || exit /b 7
+)
+
 echo Checking build directory
 IF EXIST "%INVICTUS_ROOT%\build" (
   echo "Removing build directory"
-  rmdir /Q /S "%INVICTUS_ROOT%\build" || exit /b 7
+  rmdir /Q /S "%INVICTUS_ROOT%\build" || exit /b 8
 )
 
 echo Checking packages directory: %INVICTUS_ROOT%\packages
@@ -58,10 +63,10 @@ echo Checking packages directory: %INVICTUS_ROOT%\packages
 IF EXIST "%INVICTUS_ROOT%\packages" (
   echo Removing packages directory
   del /F /Q "%INVICTUS_ROOT%\packages"
-  rmdir /Q /S "%INVICTUS_ROOT%\packages" || exit /b 8
+  rmdir /Q /S "%INVICTUS_ROOT%\packages" || exit /b 9
 )
 
-mkdir "%INVICTUS_ROOT%\packages" || exit /b 9
+mkdir "%INVICTUS_ROOT%\packages" || exit /b 10
 
 if "%1" == "" (
   set BUILD_NUMBER=0
@@ -70,18 +75,18 @@ if "%1" == "" (
 )
 
 echo Creating build directory
-mkdir "%INVICTUS_ROOT%\build" || exit /b 10
+mkdir "%INVICTUS_ROOT%\build" || exit /b 11
 pushd "%INVICTUS_ROOT%\build"
 echo "Spawning cmake generator"
-call "%INVICTUS_ROOT%\keyhotee\run_cmake.bat" -DBUILD_VERSION_PATCH=%BUILD_NUMBER% || exit /b 11
+call "%INVICTUS_ROOT%\keyhotee\run_cmake.bat" -DBUILD_VERSION_PATCH=%BUILD_NUMBER% || exit /b 12
 rem /p:VCTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110/"
 msbuild.exe /M:%NUMBER_OF_PROCESSORS% /p:Configuration=RelWithDebinfo /p:Platform=Win32 /target:rebuild /clp:ErrorsOnly keyhotee.sln
 set MSBUILD_STATUS=%ERRORLEVEL% 
-if not %MSBUILD_STATUS%==0 exit /b 12
-cpack --verbose || exit /b 13
+if not %MSBUILD_STATUS%==0 exit /b 13
+cpack --verbose || exit /b 14
 
 pushd "%INVICTUS_ROOT%/packages"
-tar -czf Keyhotee_build_%BUILD_NUMBER%.tgz *.zip *.pdb || exit /b 14
+tar -czf Keyhotee_build_%BUILD_NUMBER%.tgz *.zip *.pdb || exit /b 15
 rem to be in startup dir
 popd 
 
